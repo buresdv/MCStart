719aff8b39b4788a7bb0bec6b6f7b31aa792252a
David Bureš <buresdv@gmail.com>
2022-11-25 17:41:42 +0100

    Initial Commit

---
+++ MCStart/MCStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCStartApp.swift
+//  MCStart
+//
+//  Created by David Bureš on 25.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MCStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
